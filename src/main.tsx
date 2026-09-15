373cebf73a8fce0e6286f93dcef08fc063b2ffcc
conradipui-glitch <conradipui@gmail.com>
2026-09-15 11:32:33 +0600

    feat(v4): replace static prerender snapshot on boot

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root")!;
+if (rootElement.dataset.prerendered === "true") rootElement.replaceChildren();
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>
